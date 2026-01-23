93c6221328b0a456dbe0433f6c365bf90ba9e26b
Harsh987654 <101203635+Harsh987654@users.noreply.github.com>
2026-01-23 20:56:55 +0530

    Create  PrintVariables.java
    Add  program to print variable

---
+++ basics/PrintVariables.java
@@ -0,0 +1,12 @@
+public class PrintVariables{
+  public static void main(String[] args){
+    int age=22;
+    double cgpa=8.5;
+    String Name= "Harsh";
+
+    System.out.println("Name: "+ name);
+    System.out.println("Age: "+age);
+    System.out.println("CGPA: "+ cgpa);
+  }
+}
+    
