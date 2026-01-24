8ab900096bfe598c4970f02718c07a853b80916e
Harsh987654 <101203635+Harsh987654@users.noreply.github.com>
2026-01-25 01:09:33 +0530

    Create MethodOverloading.java
    Add MethodOverloading program

---
+++ OOP_Core/MethodOverloading.java
@@ -0,0 +1,15 @@
+Class MathOps {
+  int add(int a, int b){
+    return a+b;
+  }
+  int add(int a, int b , int c){
+    return a+b+c;
+  }
+}
+public class MethodOverloading{
+  public static void main(String[] args){
+    MathOps m = new MathOps();
+    System.out.println(m.add(2,3));
+    System.out.println(m.add(2,3,4));
+  }
+}
