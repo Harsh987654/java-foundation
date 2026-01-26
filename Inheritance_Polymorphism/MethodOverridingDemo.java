acaf05d47388411f8fcdb874212a3d3fd065a8b1
Harsh987654 <101203635+Harsh987654@users.noreply.github.com>
2026-01-26 21:56:27 +0530

    Create MethodOverridingDemo.java
    Add MethodOverridingDemo program

---
+++ Inheritance_Polymorphism/MethodOverridingDemo.java
@@ -0,0 +1,9 @@
+public class MethodOverridingDemo{
+  public static void main(String[] args){
+    Person p1 = new Student("Kumar", 102);
+    Person p2 = new Teacher("Dr. Rao","AI");
+
+  p1.display(); //Student version
+  P2.display(); //Teacher version
+  }
+}
