3a7f00591581bbc31d22f8319fea82ae5db6fc68
Harsh987654 <101203635+Harsh987654@users.noreply.github.com>
2026-01-28 23:50:37 +0530

    Create Rectangle.java
    Add this Rectangle program

---
+++ Abstraction_Interface/Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle extends Shape{
+  double length, width;
+  Rectangle(double length, double width){
+    this.length = length;
+    this.width = width;
+  }
+  double area(){
+    return length * width;
+  }
+}
