c94b5d3587e847334b08cd0ef91cc2fdb347a796
Harsh987654 <101203635+Harsh987654@users.noreply.github.com>
2026-02-21 23:44:21 +0530

    Create Main.java

---
+++ oops/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+  public static void main(String[] args){
+    Student s1 = new Student("John", 20);
+    S1.display();
+
+  Teacher t1 = new Teacher("Alice", "Math");
+    t1.display();
+
+  Bank Account acc = new Bank Account(1000);
+    acc.deposit(500);
+    acc.withdraw(200);
+
+  System.out.prinln("Final Balance: " + acc.getBalance());
+  }
+}
